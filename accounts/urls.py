c26068ebac138b6cf9c1248c180f14f20ef69468
Victor Owino <vo4685336@gmail.com>
2025-09-24 21:40:32 +0300

    add dashboard, login and register views and URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.dashboard, name="dashboard"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+]
