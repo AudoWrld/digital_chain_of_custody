ec7e9231b35bb7ace91644b02a7e8afbbde5a337
Victor Owino <vo4685336@gmail.com>
2025-09-24 21:40:32 +0300

    add dashboard, login and register views and URLs

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+@login_required
+def dashboard(request):
+    return render(request, "accounts/dashboard.html")
+
+
+def login(request):
+    return render(request, "accounts/login.html")
+
+
+def register(request):
+    return render(request, "accounts/register.html")
