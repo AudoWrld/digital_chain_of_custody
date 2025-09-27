cecce1669b16c552d482a9c7d498d5ac7a66b0fc
Demapis <debbienjenga@gmail.com>
2025-09-27 06:50:34 +0300

    first step in registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
+from django.contrib import messages
+from .models import User
+from .forms import RegisterForm
 
 
 @login_required
@@ -14,4 +15,17 @@ def login(request):
 
 
 def register(request):
-    return render(request, "accounts/register.html")
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])  # hash password
+            user.save()
+            messages.success(request, "Account created successfully!")
+            return redirect("dashboard")  # redirect after success
+        else:
+            messages.error(request, "Please correct the errors below.")
+    else:
+        form = RegisterForm()
+
+    return render(request, "accounts/register.html", {"form": form})
