46b70c4e7e2ec59c7fd8ac5e87999a6931288e87
Demapis <debbienjenga@gmail.com>
2025-09-27 05:35:28 +0300

    add User models and UserProfileManager

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,75 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+    AbstractUser,
+)
 from django.db import models
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        username = email.split("@")[0]
+        user = self.model(
+            email=email,
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            **extra_fields,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self, email, first_name, last_name, password=None, **extra_fields
+    ):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
+
+
+class User(AbstractUser):
+    ROLE = [
+        ("investigator", "Investigator"),
+        ("analyst", "Analyst"),
+        ("custodian", "Custodian"),
+        ("auditor", "Auditor"),
+        ("admin", "Admin"),
+    ]
+    first_name = models.CharField(null=False, max_length=15, blank=False)
+    last_name = models.CharField(null=False, max_length=15, blank=False)
+    username = models.CharField(null=False, unique=True, blank=False)
+    email = models.EmailField(null=False, unique=True, blank=False)
+    verified = models.BooleanField(default=False, blank=True, null=True)
+    two_factor_authentication = models.BooleanField(default=False)
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE,
+        default="investigator",
+    )
+    profile_picture = models.ImageField(
+        upload_to="profile_pics/", blank=True, null=True
+    )
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="custom_user_groups",
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="custom_user_permissions",
+        blank=True,
+    )
