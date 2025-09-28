e3da2178466031a3d5c1058467a0ce22f32fe38c
Demapis <debbienjenga@gmail.com>
2025-09-28 21:46:57 +0300

    add email verifiction and 2FA routes and views

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,15 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import User
 from .forms import RegisterForm
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from .tokens import account_activation_token
+from django.utils.encoding import force_bytes, force_str
+from django.urls import reverse
+from django.conf import settings
 
 
 @login_required
@@ -19,13 +28,63 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
+            user.is_active = False
             user.set_password(form.cleaned_data["password"])  # hash password
             user.save()
-            messages.success(request, "Account created successfully!")
-            return redirect("dashboard")  # redirect after success
+
+            current_site = get_current_site(request)
+            subject = "verify your email"
+            message = render_to_string(
+                "emails/verification_email.html",
+                {
+                    "user": user,
+                    "domain": current_site.domain,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "token": account_activation_token.make_token(user),
+                },
+            )
+
+            send_mail(
+                subject,
+                message,
+                settings.EMAIL_HOST_USER,
+                [user.email],
+                fail_silently=False,
+            )
+
+            messages.success(
+                request, "Account created! Please check your email to verify."
+            )
+            return redirect("verification_sent")  # redirect after success
         else:
             messages.error(request, "Please correct the errors below.")
     else:
         form = RegisterForm()
 
     return render(request, "accounts/register.html", {"form": form})
+
+
+def verification_sent(request):
+    return render(request, "accounts/verification_sent.html")
+
+
+def verify_email(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+        
+    
+    if user and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, "Email verified successfully! please proceed to login.")
+        return redirect("second_authentication")
+    else:
+        messages.error(request, "Verification link is invalid or has expired.")
+        return redirect("register")
+
+
+def second_authentication(request):
+    return render(request, "accounts/second_auth.html")
