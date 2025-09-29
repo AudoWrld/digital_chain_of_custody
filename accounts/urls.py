ed5bb9a4fda0c525853878779381c3b329be7d56
Victor Owino <vo4685336@gmail.com>
2025-09-29 06:24:29 +0300

    apply black formatting

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("login", views.login, name="login"),
     path("register", views.register, name="register"),
-    path("verification-sent",views.verification_sent, name="verification_sent"),
-    path("2fa",views.second_authentication, name="second_authentication"),
-    path("verify/<uidb64>/<token>/", views.verify_email, name="verify_email")
+    path("verification-sent", views.verification_sent, name="verification_sent"),
+    path("2fa", views.second_authentication, name="second_authentication"),
+    path("verify/<uidb64>/<token>/", views.verify_email, name="verify_email"),
 ]
