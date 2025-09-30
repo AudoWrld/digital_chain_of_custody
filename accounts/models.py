1bb6a1b17dc3ff7ab5a3b68be78cb23a11e356ba
Demapis <debbienjenga@gmail.com>
2025-09-30 06:16:16 +0300

    add the 2fa fields

--- accounts/models.py
+++ accounts/models.py
@@ -41,7 +41,7 @@ class UserProfileManager(BaseUserManager):
         return self.create_user(email, first_name, last_name, password, **extra_fields)
 
 
-class User(AbstractUser):
+class User(AbstractUser, PermissionsMixin):
     ROLE = [
         ("investigator", "Investigator"),
         ("analyst", "Analyst"),
@@ -74,9 +74,12 @@ class User(AbstractUser):
         blank=True,
     )
     objects = UserProfileManager()
-
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name", "role"]
+    
+    two_factor_secret = models.CharField(max_length=16, blank=True, null=True)
+    two_factor_enabled = models.BooleanField(default=False)
+    recovery_codes = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.email
