c940114357d34e0e8284e176835591d22c3d5802
Victor Owino <vo4685336@gmail.com>
2025-10-25 23:32:48 +0300

    change login to login_view

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,14 @@ from . import views
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
-    path("login", views.login, name="login"),
+    path("login", views.login_view, name="login"),
     path("register", views.register, name="register"),
-    path("verification-sent",views.verification_sent, name="verification_sent"),
-    path("2fa",views.second_authentication, name="second_authentication"),
+    path("verification-sent", views.verification_sent, name="verification_sent"),
+    path("setup-your-2fa", views.second_authentication, name="second_authentication"),
     path("verify/<uidb64>/<token>/", views.verify_email, name="verify_email"),
-    path("resend_verification", views.resend_verification_email, name="resend_verification"),
+    path(
+        "resend_verification",
+        views.resend_verification_email,
+        name="resend_verification",
+    ),
 ]
