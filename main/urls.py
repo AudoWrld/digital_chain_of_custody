26dbd58fa3ebfe8c208d910c835abbedd17658d4
Victor Owino <vo4685336@gmail.com>
2026-01-15 13:14:45 +0300

    complete main app

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.home, name="homepage"),]
+urlpatterns = [
+    path("", views.home, name="homepage"),
+    path("terms-and-condition/", views.terms, name="terms"),
+    path("privacy-policy/", views.privacy, name="privacy"),
+    path("contact/", views.contact, name="contact"),
+]
