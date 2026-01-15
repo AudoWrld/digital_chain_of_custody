3f885b870445b08c5fe31e49e70516c7595ee666
Victor Owino <vo4685336@gmail.com>
2026-01-15 13:14:45 +0300

    complete main app

--- main/views.py
+++ main/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def home(request):
     return render(request, "main/home.html")
+
+
+def contact(request):
+    return render(request, "main/contact.html")
+
+
+def terms(request):
+    return render(request, "main/terms.html")
+
+
+def privacy(request):
+    return render(request, "main/privacy.html")
